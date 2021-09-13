df53def138edf69d8d104f34fadd63f6d40cc8d6
Fenil Moradiya <fjmoradiya@gmail.com>
2021-09-13 19:15:26 +0530

    Map Types Menu Added with initial Location

--- app/src/main/java/com/digitalgenius/playwithmaps/MainActivity.kt
+++ app/src/main/java/com/digitalgenius/playwithmaps/MainActivity.kt
@@ -5,14 +5,19 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.digitalgenius.playwithmaps.databinding.ActivityMainBinding
 import com.digitalgenius.playwithmaps.utils.Functions
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var _binding: ActivityMainBinding? = null
@@ -26,6 +31,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mGoogleMap: GoogleMap
     private lateinit var supportMapFragment: SupportMapFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,10 +42,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         supportMapFragment = SupportMapFragment.newInstance()
 
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer,supportMapFragment)
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, supportMapFragment)
             .commit()
 
-        supportMapFragment.getMapAsync (this@MainActivity)
+        supportMapFragment.getMapAsync(this@MainActivity)
     }
 
     private fun initGoogleMap() {
@@ -111,6 +118,44 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+
+        goToLocation(21.269487,72.958216)
 
     }
+
+    fun goToLocation(lat:Double,lng:Double){
+        val latLng=LatLng(lat,lng)
+        val cameraUpdate=CameraUpdateFactory.newLatLngZoom(latLng,7.0f)
+        mGoogleMap.moveCamera(cameraUpdate)
+        mGoogleMap.mapType=GoogleMap.MAP_TYPE_NORMAL
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_type_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mapTypeNon->{
+                mGoogleMap.mapType=GoogleMap.MAP_TYPE_NONE
+            }
+            R.id.mapTypeNormal->{
+                mGoogleMap.mapType=GoogleMap.MAP_TYPE_NORMAL
+            }
+            R.id.mapTypeHybrid->{
+                mGoogleMap.mapType=GoogleMap.MAP_TYPE_HYBRID
+            }
+            R.id.mapTypeSatelite->{
+                mGoogleMap.mapType=GoogleMap.MAP_TYPE_SATELLITE
+            }
+            R.id.mapTypeTerrain->{
+                mGoogleMap.mapType=GoogleMap.MAP_TYPE_TERRAIN
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
